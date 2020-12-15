f4c21c7b1e8606ffad326b741faf347aa8dd3dc7
Ali <36238347+mohammadalisiddiqui@users.noreply.github.com>
2020-12-15 05:37:06 -0800

    python programming

---
+++ ali.py
@@ -0,0 +1,8 @@
+a=2.5
+b= "ali"
+c= 1
+d= "A"
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
---
+++ class2.py
@@ -0,0 +1,34 @@
+# a= "Hello Ali"
+# b = "Mehfooz"
+# c = 2
+# d = str(c)
+# print(a)
+# print(a[0:9])
+# print(a.upper())
+# print(a.lower())
+# print(a.strip())
+# print(a.replace("H","J"))
+# print(a+b)
+# print(a+" "+b+" "+d)
+
+# age = 25
+# dob = 1996
+# study = "AI"
+# txt = "My name is ali, Im {0} years old and my date of birth is {1} and i am study {2} and i am {0} years old"
+# print(txt.format(age,dob,study))
+
+# a= "We are studing \t \tAI"
+# print(a)
+# b = "We are Studing AI \nand this is 11 Class"
+# print(b)
+
+a = "We are studing AI for Robotics"
+b = int(input("Enter value of b 2 or any number "))
+
+if b == 2:
+    c= input("enter your name ")
+    print(c.upper())
+
+elif b!= 2:
+    print(len(a))
+
