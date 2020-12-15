69c0dc4a82edf292135f0ff1b67c5f223bf59eca
Ali <36238347+mohammadalisiddiqui@users.noreply.github.com>
2020-12-15 06:24:35 -0800

    python programming

--- class2.py
+++ class2.py
@@ -22,13 +22,39 @@
 # b = "We are Studing AI \nand this is 11 Class"
 # print(b)
 
-a = "We are studing AI for Robotics"
-b = int(input("Enter value of b 2 or any number "))
+# a = "We are studing AI for Robotics"
+# b = int(input("Enter value of b 2 or any number "))
 
-if b == 2:
-    c= input("enter your name ")
-    print(c.upper())
+# if b == 2:
+#     c= input("enter your name ")
+#     print(c.upper())
 
-elif b!= 2:
-    print(len(a))
+# elif b!= 2:
+#     print(len(a))
 
+# a = print(bool("hello"))
+
+# a = "Ali"
+# print("Hello "+a)
+
+# print(a+"\n"+10)
+
+my_list = ["Oil","Rice","Tapal","Dal","Dal",2,"roti"]
+print(type(my_list))
+print(my_list)
+print(len(my_list))
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+print(my_list[3])
+print(my_list[4])
+print(my_list[5])
+print(my_list[6])
+print(my_list[-2])
+print(my_list[5:6])
+if "Tapal" in my_list:
+    print("yes")
+my_list[4] = "aata"
+print(my_list)
+x =[4,5.0,"sugar"]
+print(my_list+x)
